4ce1a86a785a2803c1052f79e40ba023b3cb2764
Vsevolod <pavlovskyi.ve@gmail.com>
2021-03-23 10:14:53 +0200

    Password TextField

--- VirtualSports/View/LoginTextField.swift
+++ VirtualSports/View/LoginTextField.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 @IBDesignable
-final class LoginTextField: UITextField {
+class LoginTextField: UITextField {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,16 +29,65 @@ final class LoginTextField: UITextField {
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        setup()
+        setupBorder()
     }
 
-    internal func setup() {
+    func setup() {
+        setupBorder()
+    }
 
+    private func setupBorder() {
         bottomLine.frame = CGRect(x: 0.0, y: frame.height + 10, width: frame.width, height: 1)
         bottomLine.backgroundColor = UIColor.black.withAlphaComponent(0.6).cgColor
 
         borderStyle = .none
         layer.addSublayer(bottomLine)
+    }
+
+}
+
+@IBDesignable
+final class PasswordLoginTextField: LoginTextField {
+
+    var button = UIButton(type: .custom)
+
+    override func setup() {
+        super.setup()
+
+        isSecureTextEntry = true
+        setupVisibilityButton()
+    }
+
+    private func setupVisibilityButton() {
+
+        button.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+
+        button.contentHorizontalAlignment = .fill
+        button.contentVerticalAlignment = .fill
+        button.imageView?.contentMode = .scaleAspectFit
+
+        button.addTarget(self, action: #selector(didTapVisibilityButton), for: .touchUpInside)
+
+        rightView = button
+        rightViewMode = .always
+        updateVisibilityButton()
+    }
+
+    private func updateVisibilityButton() {
+
+        if isSecureTextEntry {
+            button.setImage(UIImage(named: "EyeCrossed"), for: .normal)
+            button.alpha = 0.5
+        } else {
+            button.setImage(UIImage(named: "Eye"), for: .normal)
+            button.alpha = 1
+        }
+    }
+
+    @objc
+    private func didTapVisibilityButton() {
+        togglePasswordVisibility()
+        updateVisibilityButton()
     }
 
 }
