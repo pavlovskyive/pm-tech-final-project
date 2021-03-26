4677e693764ed21f39a99316ebd19eead81d9523
Alexandr Sopilnyk <sopilnyak.alexandr@gmail.com>
2021-03-26 16:01:05 +0200

    Add Network Monitor + offlineViewController

---
+++ VirtualSports/Controller/OfflineViewController/OfflineViewController.swift
@@ -0,0 +1,8 @@
+//
+//  OfflineViewController.swift
+//  VirtualSports
+//
+//  Created by Alexandr Sopilnyak on 26.03.2021.
+//
+
+import Foundation
---
+++ VirtualSports/Helper/NetworkMonitor.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkMonitor.swift
+//  VirtualSports
+//
+//  Created by Alexandr Sopilnyak on 25.03.2021.
+//
+
+import Foundation
